674cfe9a211a03c48378957e854eb9dc458b0887
Mirade McGloin <mirade.mcgloin@gmail.com>
2020-09-06 19:15:46 -0600

    still trying

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Workout = require("../models/Workout")
+const Workout = require("./models/Workout.js")
 
 router.get("/api/workouts", (req, res) => {
     Workout.find({})
